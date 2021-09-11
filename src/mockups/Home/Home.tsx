a91ba5165ada0db864442c3cec858ed0d145a1f7
Bibek Saini <itsbibeksaini@gmail.com>
2021-09-11 22:54:06 +0530

    Mockups home grid view

--- src/mockups/Home/Home.tsx
+++ src/mockups/Home/Home.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import { Box } from '@material-ui/core';
+import React, { Component } from 'react';
 import styles from './Home.module.scss';
+import logo from '../../assets/loginpage_dark.png'
 
-const Home = () => (
-  <div className={styles.Home}>
-    Home Component
-  </div>
-);
-
-export default Home;
+export default class Home extends Component{
+  render(){
+    return (
+      <Box display="flex" className={`${styles.padding2030} ${styles.galleryWrapper}`}>
+        <div className={`${styles.grid}`}>
+          <div className={`${styles.gridItem} ${styles.gridItemLG}`}>
+            <img src={logo} alt=""></img>
+          </div>
+        </div>
+      </Box>
+    )
+  }
+}
