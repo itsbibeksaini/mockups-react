56b542d8c8b8e0e0b07f4c1be5284d268d4bc598
Bibek Saini <itsbibeksaini@gmail.com>
2021-09-15 13:19:30 +0530

    Adding mockups draft for music store

--- src/mockups/MusicStore/MusicStore.tsx
+++ src/mockups/MusicStore/MusicStore.tsx
@@ -1,9 +1,64 @@
+import { Box } from '@material-ui/core';
 import React from 'react';
 import styles from './MusicStore.module.scss';
 
 const MusicStore = () => (
-  <div className={styles.MusicStore}>
-    MusicStore Component
+  <div className={`${styles.MusicStore}`}>    
+    <header className={`${styles.accordian}`}>
+      {/* Accordian slider */}
+      <ul>
+        <li>
+          <div className={`${styles.imageTitle}`}>
+            <span>KungFu Panda</span>
+          </div>
+          <span>
+            <img src="http://thecodeplayer.com/uploads/media/3yiC6Yq.jpg"/>
+          </span>
+        </li>
+        <li>
+          <div className={`${styles.imageTitle}`}>
+            <span>KungFu Panda</span>
+          </div>
+          <span>
+            <img src="http://thecodeplayer.com/uploads/media/3yiC6Yq.jpg"/>
+          </span>
+        </li>
+        <li>
+          <div className={`${styles.imageTitle}`}>
+            <span>KungFu Panda</span>
+          </div>
+          <span>
+            <img src="http://thecodeplayer.com/uploads/media/3yiC6Yq.jpg"/>
+          </span>
+        </li>
+        <li>
+          <div className={`${styles.imageTitle}`}>
+            <span>KungFu Panda</span>
+          </div>
+          <span>
+            <img src="http://thecodeplayer.com/uploads/media/3yiC6Yq.jpg"/>
+          </span>
+        </li>
+        <li>
+          <div className={`${styles.imageTitle}`}>
+            <span>KungFu Panda</span>
+          </div>
+          <span>
+            <img src="http://thecodeplayer.com/uploads/media/3yiC6Yq.jpg"/>
+          </span>
+        </li>
+      </ul>
+    </header>
+    <hr/>
+    <Box display="flex" flexWrap="wrap" style={{border:'1px solid', width:'800px', margin:'auto'}} className={`${styles.albumArts}`}>
+      <Box m={1} className={`${styles.album}`}>
+
+      </Box>
+      <Box m={1} className={`${styles.album}`}>
+
+      </Box>
+      
+    </Box>
   </div>
 );
 
