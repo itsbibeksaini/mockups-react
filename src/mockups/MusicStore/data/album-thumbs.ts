251a5a9530825e819686755b62205c9e6821b6c7
Bibek Saini <itsbibeksaini@gmail.com>
2021-09-15 23:15:45 +0530

    more album art

--- src/mockups/MusicStore/data/album-thumbs.ts
+++ src/mockups/MusicStore/data/album-thumbs.ts
@@ -4,6 +4,11 @@ import airlift from '../../../assets/music-store/thumbs/airlift.png'
 import kabirSingh from '../../../assets/music-store/thumbs/kabir-singh.png'
 import roy from '../../../assets/music-store/thumbs/roy.png'
 import wazir from '../../../assets/music-store/thumbs/wazir.png'
+import befikre from '../../../assets/music-store/thumbs/befikre.png'
+import dishoom from '../../../assets/music-store/thumbs/dishoom.png'
+import jaggaJasoos from '../../../assets/music-store/thumbs/jagga-jasoos.png'
+import raazReboot from '../../../assets/music-store/thumbs/raaz-reboot.png'
+import rockeyHandsome from '../../../assets/music-store/thumbs/rockey-handsome.png'
 
 export const ALBUM_THUMBS:IAlbum[] = [{
     image:alliswell,
@@ -20,4 +25,19 @@ export const ALBUM_THUMBS:IAlbum[] = [{
 },{
     image:wazir,
     title: 'Wazir'
+} ,{
+    image:befikre,
+    title: 'Befikre'
+} ,{
+    image:dishoom,
+    title: 'Dishoom'
+},{
+    image:jaggaJasoos,
+    title: 'Jagga Jasoos'
+},{
+    image:raazReboot,
+    title: 'Raaz Reboot'
+},{
+    image:rockeyHandsome,
+    title: 'Rockey Handsome'
 }]
