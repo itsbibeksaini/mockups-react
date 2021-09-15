8490446708044456d2057a9886e10b29c91b9ba2
Bibek Saini <itsbibeksaini@gmail.com>
2021-09-15 23:34:13 +0530

    Update

--- src/mockups/Home/data/thumbnail-data.ts
+++ src/mockups/Home/data/thumbnail-data.ts
@@ -1,12 +1,13 @@
 import IThumbnail from "../state/IThumbnail";
-import loginpage from '../../../assets/loginpage.png'
+import loginpage from '../../../assets/thumbs/loginpage.png'
+import musicstore from '../../../assets/thumbs/musicstore.png'
 
 export const THUMBNAIL_DATA:IThumbnail[] = [{
     image: loginpage,
     routerLink: '/login',
     size:''
 }, {
-    image: loginpage,
+    image: musicstore,
     routerLink: '/music-store',
     size:''
 }]
