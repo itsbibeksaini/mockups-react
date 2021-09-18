d4084970d161b047a2b3136fdc3619af3a5e39ca
Bibek Saini <itsbibeksaini@gmail.com>
2021-09-18 21:22:52 +0530

    Profileview draft mockup

--- src/core/routes/routes.ts
+++ src/core/routes/routes.ts
@@ -2,6 +2,7 @@ import MainLayout from "../../components/MainLayout/MainLayout";
 import Home from "../../mockups/Home/Home";
 import LoginPage from "../../mockups/LoginPage/LoginPage";
 import MusicStore from "../../mockups/MusicStore/MusicStore";
+import ProfileView from "../../mockups/ProfileView/ProfileView";
 import IRoute from "./IRoute";
 
 const ROUTES: IRoute[] = [{
@@ -29,6 +30,17 @@ const ROUTES: IRoute[] = [{
         path: '',
         name: "Music Store",
         component: MusicStore,
+        exact: true,
+    }]
+}, {
+    path: '/profile-view',
+    name: "",
+    component: MainLayout,
+    exact: true,
+    childern: [{
+        path: '',
+        name: "Profile view",
+        component: ProfileView,
         exact: true,
     }]
 }]
--- src/mockups/ProfileView/ProfileView.tsx
+++ src/mockups/ProfileView/ProfileView.tsx
@@ -1,10 +1,35 @@
+import { Typography } from '@material-ui/core';
 import React from 'react';
 import styles from './ProfileView.module.scss';
 
-const ProfileView = () => (
-  <div className={styles.ProfileView}>
-    ProfileView Component
-  </div>
-);
+class ProfileView extends React.Component{
+  render(){
+    return(
+      <div className={`${styles.ProfileView}`}>       
+        <header>
+          <div className={`${styles.profileIcon}`}>
+              <span className={`${styles.initials}`}>JD</span>
+          </div>
+        </header>
+
+        <div className={`${styles.userName}`}>
+            <Typography variant="h3">John Doe</Typography>
+            <Typography variant="subtitle1">@johndoe</Typography>
+        </div>
+
+        <div className={`${styles.description}`}>
+            <Typography variant="h6">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </Typography>
+            <hr/>
+        </div>
+ 
+        <div style={{border:'1px solid'}}>
+
+        </div>
+      </div>
+    )
+  }
+}
 
 export default ProfileView;
