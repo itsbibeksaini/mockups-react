e8a93708724ad8076c9fb941eaeceacc82b7c50e
Bibek Saini <itsbibeksaini@gmail.com>
2021-09-19 15:44:49 +0530

    Adding edit button to description

--- src/mockups/ProfileView/ProfileView.tsx
+++ src/mockups/ProfileView/ProfileView.tsx
@@ -1,5 +1,7 @@
 
-import { Tab, Tabs, Typography } from '@mui/material';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Button, IconButton, Tab, Tabs, Typography } from '@mui/material';
 import React from 'react';
 import styles from './ProfileView.module.scss';
 import IProfileViewState from './state/IProfileViewState';
@@ -28,17 +30,24 @@ class ProfileView extends React.Component<{}, IProfileViewState>{
         </div>
 
         <div className={`${styles.description}`}>
+          <div style={{position:'relative'}}>
+            <div className={`${styles.edit}`}>
+              <IconButton color="primary">
+                <FontAwesomeIcon icon={faPencilAlt} style={{fontSize:'21px', color:'#000'}} />
+              </IconButton>              
+            </div>
             <Typography variant="h6">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </Typography>
-            <hr/>
+            </Typography>            
+          </div>          
+          <hr/>
         </div>
  
         <div>
         <Tabs  value={this.state.value} onChange={this.switchTab.bind(this)} centered>
-          <Tab label="Item One" id={`simple-tab-0`}/>
-          <Tab label="Item Two" />
-          <Tab label="Item Three" />
+          <Tab label="Account" id={`simple-tab-0`}/>
+          <Tab label="Permissions" />
+          <Tab label="History" />
         </Tabs>        
         <div id={`simple-tabpanel-0`}>
 
