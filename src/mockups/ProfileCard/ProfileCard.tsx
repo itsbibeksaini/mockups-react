ec296a29f61c3b625f0ccef3889d4c98dd47ef41
Bibek Saini <itsbibeksaini@gmail.com>
2021-09-21 00:15:19 +0530

    Update

--- src/mockups/ProfileCard/ProfileCard.tsx
+++ src/mockups/ProfileCard/ProfileCard.tsx
@@ -8,26 +8,27 @@ class ProfileCard extends React.Component{
   render(){
     return(
       <Box display="flex">
-        <Box style={{border:'2px solid', width:'280px', padding:'20px 30px', borderRadius:'0.5rem', position:'relative', margin:'auto'}}>
-          <div style={{position:'absolute', top:'5px', right:'5px'}}>
+        <Box className={`${styles.ProfileCard}`}>
+          <div className={`${styles.gearOption}`}>
             <IconButton color="primary">
               <FontAwesomeIcon icon={faCog} style={{fontSize:"18px", color:'#000'}}/>
             </IconButton>
           </div>
           <header>
-            <div style={{width:'100px', height:'100px', borderRadius:'50%', border:'1px solid', margin:'auto'}}>
+            <div className={`${styles.profileIcon}`}>
+              <span className={`${styles.initials}`}>JD</span>
             </div>
           </header>
           <div style={{textAlign:'center'}}>
             <Typography variant="h4">John Doe</Typography>
-            <Typography variant="subtitle1">@johndoe</Typography>
+            <Typography variant="subtitle1">@johndoe</Typography>            
           </div>
 
-          <div style={{padding:'10px 15px'}}>
+          <div className={`${styles.profileIntro}`}>
             <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Typography>
           </div>
 
-          <Box display='flex' style={{padding:'5px 10px'}}>
+          <Box display='flex' className={`${styles.profileActions}`}>
             <Box flexGrow={0.5} p={0.5}>
               <Button variant='outlined' style={{width:'100%'}} color='error'>
                 Delete
