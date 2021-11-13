f49d0730ae6bb3434ee4f16d41c77574e675f5d4
Bibek Saini <itsbibeksaini@gmail.com>
2021-11-13 23:37:40 +0530

    Onboarding get started page

--- src/mockups/OnboardingFlow/OnboardingFlow.tsx
+++ src/mockups/OnboardingFlow/OnboardingFlow.tsx
@@ -1,7 +1,8 @@
-import { Step, StepButton, StepLabel, Stepper } from '@mui/material';
+import { Button, Step, StepButton, StepLabel, Stepper, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
 import styles from './OnboardingFlow.module.scss';
+import onboard from '../../assets/illustrations/onboard.svg';
 
 
 export class OnboardingFlow extends React.Component {
@@ -24,6 +25,19 @@ export class OnboardingFlow extends React.Component {
               })
             }
           </Stepper>
+
+          <div style={{padding:'20px 30px', textAlign:'center'}}>
+            <Typography variant="h3" gutterBottom>Welcome John Doe</Typography>
+            <img src={onboard} alt="Onboarding flow" style={{width:'500px'}} />
+
+            <div>
+              <Typography variant="h4" gutterBottom>We are so excited to have you onboard</Typography>
+              <Typography variant="body1" gutterBottom>We are here to help you get started with your new account. We will guide you through the process and we will be there for you if you need any help.</Typography>
+            </div> 
+            <div>
+              <Button variant="contained" color="primary" style={{marginTop:'20px'}}>Let's get started</Button>
+            </div>
+          </div>
       </Box>
     );
   }
