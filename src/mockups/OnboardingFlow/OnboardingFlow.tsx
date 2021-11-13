abe9716ddbea1c7b2e9d55d1f3ac325558b3de7e
Bibek Saini <itsbibeksaini@gmail.com>
2021-11-13 22:33:13 +0530

    Update

--- src/mockups/OnboardingFlow/OnboardingFlow.tsx
+++ src/mockups/OnboardingFlow/OnboardingFlow.tsx
@@ -1,4 +1,4 @@
-import { Step, StepLabel, Stepper } from '@mui/material';
+import { Step, StepButton, StepLabel, Stepper } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
 import styles from './OnboardingFlow.module.scss';
@@ -10,13 +10,15 @@ export class OnboardingFlow extends React.Component {
 
   render() {
     return (
-      <Box sx={{ width: '100%' }}>
+      <Box sx={{ width: '100%' }} style={{padding:'10px 15px', boxSizing:'border-box'}}>
           <Stepper>
             {
               this.steps.map((label, index) => {
                 return (
                   <Step key={label}>
-                    <StepLabel >{label}</StepLabel>
+                    <StepButton color="inherit">
+                      {label}
+                    </StepButton>
                   </Step>
                 )
               })
