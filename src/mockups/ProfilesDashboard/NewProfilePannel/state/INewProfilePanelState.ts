2c2c4e37e64defbbdd15adc0482812e92fa5b50e
Bibek Saini <itsbibeksaini@gmail.com>
2022-02-20 17:37:53 +0530

    Updating new profile panel state
    Adding new profile interface

--- src/mockups/ProfilesDashboard/NewProfilePannel/state/INewProfilePanelState.ts
+++ src/mockups/ProfilesDashboard/NewProfilePannel/state/INewProfilePanelState.ts
@@ -1,6 +1,17 @@
 interface INewProfilePanelState {
     value: number,
-    language: string
+    newProfile: INewProfileForm
+}
+
+interface INewProfileForm{
+    username: string,
+    first_name: string,
+    last_name: string,
+    password:string
+    confirm_password:string
+    language: string,
+    email:string
+    
 }
 
 export default INewProfilePanelState;
