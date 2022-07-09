a1e6d68d8fa1e3f8e47cc3283f4667d242a24022
Bibek Saini <itsbibeksaini@gmail.com>
2022-07-09 15:39:35 +0530

    basic skeleton

--- src/mockups/Worklogs/WorklogsDashboard/WorklogsDashboard.tsx
+++ src/mockups/Worklogs/WorklogsDashboard/WorklogsDashboard.tsx
@@ -1,12 +1,18 @@
+import { Box } from '@mui/system';
 import React, { FC } from 'react';
 import styles from './WorklogsDashboard.module.scss';
 
 interface WorklogsDashboardProps {}
 
-const WorklogsDashboard: FC<WorklogsDashboardProps> = () => (
-  <div className={styles.WorklogsDashboard}>
-    WorklogsDashboard Component
-  </div>
-);
+class WorklogsDashboard extends React.Component<WorklogsDashboardProps> {
+  render() {
+    return (
+      <Box display="flex" justifyContent='center' className={styles.WorklogsDashboard}>
+        <Box m={1.5} flexDirection="column" display="flex" className={`${styles.worklogCard}`}>          
+        </Box>                      
+      </Box>
+    )
+  }
+}
 
 export default WorklogsDashboard;
