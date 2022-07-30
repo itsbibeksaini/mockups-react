d844bfa8c4dad884f95f9d4b4fa6daa9ed67c293
Bibek Saini <itsbibeksaini@gmail.com>
2022-07-30 12:45:29 +0530

    chore: worklog cards

--- src/mockups/Worklogs/WorklogsDashboard/WorklogsDashboard.tsx
+++ src/mockups/Worklogs/WorklogsDashboard/WorklogsDashboard.tsx
@@ -1,3 +1,6 @@
+import { faCamera, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconButton, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React, { FC } from 'react';
 import styles from './WorklogsDashboard.module.scss';
@@ -5,14 +8,35 @@ import styles from './WorklogsDashboard.module.scss';
 interface WorklogsDashboardProps {}
 
 class WorklogsDashboard extends React.Component<WorklogsDashboardProps> {
+  
+  constructor(props: WorklogsDashboardProps){
+    super(props)
+  }
+  
   render() {
     return (
-      <Box display="flex" justifyContent='center' className={styles.WorklogsDashboard}>
-        <Box m={1.5} flexDirection="column" display="flex" className={`${styles.worklogCard}`}>          
-        </Box>                      
+      <Box display="flex" justifyContent='center' className={styles.WorklogsDashboard}>        
+        { this.worklogCard() }
+      </Box>
+    )
+  }
+
+  worklogCard(){
+    return (
+      <Box m={1.5} flexDirection="column" display="flex" className={`${styles.worklogCard}`}>
+        <IconButton className={`${styles.deleteIcon}`}>
+          <FontAwesomeIcon icon={faTrash} style={{fontSize:'14px', color:'red'}}/>
+        </IconButton>
+        <div className={`${styles.title}`}>
+          <Typography variant='h5'>
+            Worklog name
+          </Typography>
+        </div>
       </Box>
     )
   }
 }
+
+
 
 export default WorklogsDashboard;
