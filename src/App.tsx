7751208274f9ed6ffc64023edca28cb251b140fc
Bibek Saini <itsbibeksaini@gmail.com>
2023-10-01 19:49:02 -0400

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,19 @@ import ROUTES from './core/routes/routes';
 
 export default class App extends React.Component{
 
-  // constructor(props:any){
-  //   super(props)
-  // }
-
   render(){
     return (
       <HashRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           {ROUTES.map((route, index) => {
             return(
-              <Route 
-                key={index} 
-                path={route.path} 
-                // Component={}
-                // render={(props: RouteComponentProps<any>) => (
-                //   <route.component routes={route.childern} {...props} {...route.props}/>
-                // )} 
-              />
+              <Route key={index} path={route.path} element={<route.component/>}>
+                {route.childern?.map((childRoute, childIndex) => {
+                  return (
+                    <Route key={childIndex} path={childRoute.path} element={<childRoute.component/>} />
+                  )
+                })}
+              </Route>
             )
           })}
         </Routes>
--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,49 +1,37 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Outlet } from 'react-router-dom';
 import styles from './MainLayout.module.scss';
 import IRoute from '../../core/routes/IRoute';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons';
 import SavedUser from '../../mockups/LoginPage/state/ISavedUser';
 import { Box, Card } from '@mui/material';
-class MainLayout extends React.Component<{}, SavedUser>{
+class MainLayout extends React.Component<{},{}>{
 
-  routes: IRoute[];
   constructor(props:any){    
     super(props)    
-    this.routes = props.routes    
-
-    
   }
 
   render(){
     return(
       <div style={{padding:'0.75rem', 'height': '100%', boxSizing: 'border-box'}}>        
-        <Card style={{boxSizing:'border-box', margin:'auto', height:'100%', overflow:'hidden', overflowY:'auto'}} classes={{root: 'shadow max-supported-width'}}>          
-
-        {this.routes.map((route, index) => {                    
-          return (
-            <Box display="flex" flexDirection="column" key={index} style={{height:'100%'}}>            
+        <Card style={{boxSizing:'border-box', margin:'auto', height:'100%', overflow:'hidden', overflowY:'auto'}} 
+        classes={{root: 'shadow max-supported-width'}}>          
+          <Box display="flex" flexDirection="column" style={{height:'100%'}}>            
               <Box className={'padding2030'} style={{paddingBottom:'0'}}>
                 <Link to="/">
                   <FontAwesomeIcon icon={faLongArrowAltLeft} style={{fontSize:'28px', color:'#000'}} className={'cursor-pointer'} />  
                 </Link>
-                <h2 style={{margin:0}}>{route.name}</h2>
+                {/* <h2 style={{margin:0}}>{route.name}</h2> */}
                 <hr/>
               </Box>
               
 
               <Box className={`${styles.pageWrapper}`} sx={{ flexGrow: 1 }}>
-                <Route path={route.path}
-                      // exact={route.exact}
-                      // render={(props: RouteComponentProps<any>) => (
-                      //   <route.component routes={route.childern} {...props} {...route.props}/>
-                      // )}
-                       />
+                <Outlet/>
               </Box>
             </Box>
-          )
-        })}
+        
           
         </Card>
       </div>
@@ -52,3 +40,4 @@ class MainLayout extends React.Component<{}, SavedUser>{
 }
 
 export default MainLayout;
+ 
