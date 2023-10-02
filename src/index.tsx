c97e32eba36ed906cfba2d786d2baa8f9dc0c5b2
Bibek Saini <itsbibeksaini@gmail.com>
2023-10-01 20:05:14 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'animate.css/animate.min.css';
 
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
